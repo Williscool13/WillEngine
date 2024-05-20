7db22058a097a6c7230add41f7ade7ecf2691309
Williscool13 <twtw40@gmail.com>
2024-05-20 17:23:50 +0700

    skeleton code from vkguide.dev. Vulkan integrated and tested.

---
+++ WillEngine/src/main.cpp
@@ -0,0 +1,14 @@
+#include <vk_engine.h>
+#include <iostream>
+int main(int argc, char* argv[])
+{
+	VulkanEngine engine;
+
+	engine.init();	
+	
+	engine.run();	
+
+	engine.cleanup();	
+
+	return 0;
+}
